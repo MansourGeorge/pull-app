76670690319b6d497d3ee9e47db94680680848cf
MansourGeorge <kgmcomp@hotmail.com>
2026-03-17 15:57:20 +0200

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -40,3 +40,10 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
